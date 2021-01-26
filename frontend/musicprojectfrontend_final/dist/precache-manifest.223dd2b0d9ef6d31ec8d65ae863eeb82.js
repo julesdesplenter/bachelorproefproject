2a4fe9ee5d6a0b95e5e12e27383002599929fdb1
jules <jules.desplenter@student.howest.be>
2021-01-26 11:50:13 +0100

    fixed visbility dist

---
+++ frontend/musicprojectfrontend_final/dist/precache-manifest.223dd2b0d9ef6d31ec8d65ae863eeb82.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b62390a4890f606cfd",
+    "url": "/musicproject/css/Home.7bece51f.css"
+  },
+  {
+    "revision": "27dcf62ae21f4672c762",
+    "url": "/css/NewInstrument.a721acb3.css"
+  },
+  {
+    "revision": "961d80024d14821f254a",
+    "url": "/css/NewSong.7e0a04fd.css"
+  },
+  {
+    "revision": "17a7c7d029ca72c5166b",
+    "url": "/css/UploadSong.403af3c7.css"
+  },
+  {
+    "revision": "0ece59ed02dd4d00d65c",
+    "url": "/musicproject/css/app.0d6df1f0.css"
+  },
+  {
+    "revision": "f523ee59642e123bb1bd9bd9fda8ea8d",
+    "url": "/img/casette.f523ee59.jpg"
+  },
+  {
+    "revision": "ade8e18188c6bdd7ef812ee1c167b79f",
+    "url": "/img/drum.ade8e181.jpg"
+  },
+  {
+    "revision": "287aae277e396f0b9d266feaa3b768e3",
+    "url": "/img/guitar.287aae27.jpg"
+  },
+  {
+    "revision": "f0b20e3e64d2e9bfeb781304cb7a077d",
+    "url": "/img/newSong.f0b20e3e.jpg"
+  },
+  {
+    "revision": "3cba0f186673601b5f435a5e7aa4dfea",
+    "url": "/img/piano.3cba0f18.jpg"
+  },
+  {
+    "revision": "81ebbe04a6f44d26816d6f121981ead7",
+    "url": "/img/synth.81ebbe04.jpg"
+  },
+  {
+    "revision": "b09cf2b7cddd2dee34161db5679aeb3a",
+    "url": "/img/trumpet.b09cf2b7.jpg"
+  },
+  {
+    "revision": "9c129fadc437aa81bc1c30beb1b132b1",
+    "url": "/img/uploadsong.9c129fad.jpg"
+  },
+  {
+    "revision": "3f86fcf9aeb3cc8f0acaa1f5c180e814",
+    "url": "/img/violin.3f86fcf9.jpg"
+  },
+  {
+    "revision": "3cfe91477c309fba1e6dbdb1e93e52e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e47e435555169fb9670b",
+    "url": "/js/EditSong.67cc815d.js"
+  },
+  {
+    "revision": "387a5da24757e3f382da",
+    "url": "/js/EditSong~NewSong.1e7ccd72.js"
+  },
+  {
+    "revision": "69b62390a4890f606cfd",
+    "url": "/js/Home.3bfedce2.js"
+  },
+  {
+    "revision": "27dcf62ae21f4672c762",
+    "url": "/js/NewInstrument.35b0df33.js"
+  },
+  {
+    "revision": "961d80024d14821f254a",
+    "url": "/js/NewSong.599de96b.js"
+  },
+  {
+    "revision": "5b8a65f2a10d93286554",
+    "url": "/js/SM.f682a15b.js"
+  },
+  {
+    "revision": "c97869f07f9144b13f70",
+    "url": "/js/UP.49e51cfa.js"
+  },
+  {
+    "revision": "17a7c7d029ca72c5166b",
+    "url": "/js/UploadSong.312f275d.js"
+  },
+  {
+    "revision": "0ece59ed02dd4d00d65c",
+    "url": "/js/app.f775d311.js"
+  },
+  {
+    "revision": "810405227eef0f8ca136",
+    "url": "/js/chunk-vendors.78b72aa5.js"
+  },
+  {
+    "revision": "52a06b0b320096e4905ba0d3d589d869",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
